0c3861f8fe3429a5af1382aec048398e4ddefe8f
Yandri <zhyaisme@gmail.com>
2022-04-06 23:08:56 +0700

    Modifying views.py

--- blog/views.py
+++ blog/views.py
@@ -7,4 +7,5 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by(
         "published_date"
     )
+    print(posts)
     return render(request, "blog/post_list.html", {"posts": posts})
