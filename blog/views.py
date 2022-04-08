54246eb9ccb731086a201f873e30dd1a580c2c07
Yandri <zhyaisme@gmail.com>
2022-04-08 21:07:12 +0700

    CSS and template extensions

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 
@@ -9,5 +10,12 @@ def post_list(request):
     )
     print(posts)
     return render(
-        request, "blog/post_list.html", {"posts": posts, "number": len(posts), "constant":1}
+        request,
+        "blog/post_list.html",
+        {"posts": posts, "number": len(posts), "constant": 1},
     )
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, "blog/post_detail.html", {"post": post})
